21abe40717bd72370f190b6403c230bb0fd3add1
Pontus <pontus@minabastapolare.se>
2019-04-07 00:34:35 +0200

    change public dir

--- src/server/server.js
+++ src/server/server.js
@@ -4,7 +4,7 @@ const http = require('http').Server(app);
 const io = require('socket.io')(http, { origins: '*:*'});
 const port = process.env.PORT || 3000;
 
-app.use(express.static('public'));
+app.use(express.static('build'));
 
 io.on('connection', function(socket) {
   const username = Math.random().toString(36).substring(7);
