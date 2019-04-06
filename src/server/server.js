3d4a7f344a1c7cf5f8931653194986cf1c85e53e
Pontus <pontus@minabastapolare.se>
2019-04-07 00:29:47 +0200

    Port from env

--- src/server/server.js
+++ src/server/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const http = require('http').Server(app);
 const io = require('socket.io')(http, { origins: '*:*'});
+const port = process.env.PORT || 3000;
 
 app.use(express.static('public'));
 
@@ -24,6 +25,6 @@ io.on('connection', function(socket) {
   });
 });
 
-http.listen(3000, function() {
-  console.log('listening on *:3000');
+http.listen(port, function() {
+  console.log(`listening on *:${port}`);
 });
